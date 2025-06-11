1d516a7a3257aa685bb8f6ae0c421ad249206bac
Alexis Lucattini <Alexis.Lucattini@umccr.org>
2025-06-11 10:01:34 +1000

    Shorten stack name, otherwise asset names are too long

--- bin/deploy.ts
+++ bin/deploy.ts
@@ -12,7 +12,7 @@ if (!deployMode) {
 }
 
 if (deployMode === 'stateless') {
-  new StatelessDeployStack(app, 'BSSHToAWSS3CopyManagerStatelessDeploymentPipeline', {
+  new StatelessDeployStack(app, 'BSSHToAWSS3CopyStatelessPipeline', {
     env: TOOLCHAIN_ENVIRONMENT,
   });
 } else {
--- infrastructure/toolchain/stateless-deploy-stack.ts
+++ infrastructure/toolchain/stateless-deploy-stack.ts
@@ -9,11 +9,11 @@ export class StatelessDeployStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    new DeploymentStackPipeline(this, 'BSSHToAWSS3CopyManagerStatelessDeploymentPipeline', {
+    new DeploymentStackPipeline(this, 'BSSHToAWSS3CopyStatelessPipeline', {
       githubBranch: 'main',
       githubRepo: REPO_NAME,
       stack: StatelessApplicationStack,
-      stackName: 'BSSHToAWSS3CopyManagerStatelessDeployStack',
+      stackName: 'BSSHToAWSS3CopyStatelessDeployStack',
       stackConfig: {
         beta: getStatelessStackProps('BETA'),
         gamma: getStatelessStackProps('GAMMA'),

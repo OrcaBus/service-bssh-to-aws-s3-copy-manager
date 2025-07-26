0213b9ac2ade564dbac3662a47c5dd0698512da2
Alexis Lucattini <alexis.lucattini@umccr.org>
2025-07-26 17:25:01 +1000

    Use new platform cdk constructs sequence run manager tools services (#15)

--- app/lambdas/get_library_objects_from_samplesheet_py/get_library_objects_from_samplesheet.py
+++ app/lambdas/get_library_objects_from_samplesheet_py/get_library_objects_from_samplesheet.py
@@ -8,7 +8,7 @@ Get the libraries from the samplesheet
 from typing import Dict, List
 
 # Layer imports
-from orcabus_api_tools.sequence import get_library_ids_in_sequence, get_sequence_object_from_instrument_run_id
+from orcabus_api_tools.sequence import get_libraries_from_instrument_run_id
 from orcabus_api_tools.metadata import get_libraries_list_from_library_id_list
 
 
@@ -23,11 +23,8 @@ def handler(event, context) -> Dict[str, List[Dict[str, str]]]:
     # Get the inputs
     instrument_run_id = event["instrumentRunId"]
 
-    # Get the sequence object orcabus id from the instrument run id
-    sequence_orcabus_id = get_sequence_object_from_instrument_run_id(instrument_run_id)['orcabusId']
-
     # Get the library id list
-    library_id_list = list(set(get_library_ids_in_sequence(sequence_orcabus_id)))
+    library_id_list = list(sorted(set(get_libraries_from_instrument_run_id(instrument_run_id))))
 
     # Get the library objects
     library_objects = list(map(
@@ -43,3 +40,31 @@ def handler(event, context) -> Dict[str, List[Dict[str, str]]]:
     return {
         "libraryObjects": library_objects
     }
+
+
+# if __name__ == "__main__":
+#     import json
+#     from os import environ
+#     environ['AWS_PROFILE'] = 'umccr-production'
+#     environ['AWS_REGION'] = 'ap-southeast-2'
+#     environ['HOSTNAME_SSM_PARAMETER_NAME'] = '/hosted_zone/umccr/name'
+#     environ['ORCABUS_TOKEN_SECRET_ID'] = 'orcabus/token-service-jwt'
+#     print(json.dumps(
+#         handler(
+#             {
+#                 "instrumentRunId": "250724_A01052_0269_AHFHWJDSXF"
+#             },
+#             None
+#         ),
+#         indent=4
+#     ))
+#
+#     # {
+#     #     "libraryObjects": [
+#     #         {
+#     #             "libraryId": "L2500872",
+#     #             "orcabusId": "lib.01K0P4YPP6NJ5R14P75WT94JB9"
+#     #         },
+#     #         ... (more libraries)
+#     #     ]
+#     # }

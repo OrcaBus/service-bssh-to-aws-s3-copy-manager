420bd91485a25604fbf269c5489264db99fb18aa
Alexis Lucattini <alexis.lucattini@umccr.org>
2026-03-25 13:45:11 +1100

    Strip down bssh tools lambda layer (#36)
    Lambda layer also assumes adjacent icav2 tools layer is added to lambda

--- app/layers/bssh_manager_tools_layer/src/bssh_manager_tools/utils/icav2_analysis_helpers.py
+++ app/layers/bssh_manager_tools_layer/src/bssh_manager_tools/utils/icav2_analysis_helpers.py
@@ -15,8 +15,9 @@ from wrapica.libica_models import (
 )
 from wrapica.project_data import (
     find_project_data_bulk,
-    get_file_by_file_name_from_project_data_list, get_project_data_obj_by_id,
-    get_project_data_folder_id_from_project_id_and_path, list_project_data_non_recursively
+    get_project_data_obj_by_id,
+    get_project_data_folder_id_from_project_id_and_path,
+    list_project_data_non_recursively
 )
 from wrapica.project_analysis import (
     get_analysis_input_object_from_analysis_input_code,
@@ -85,11 +86,14 @@ def get_bclconvert_outputs_from_analysis_id(
     analysis_id: str
 ) -> Tuple[str, List[ProjectData]]:
 
-    bclconvert_output_folder_id = get_analysis_output_object_from_analysis_output_code(
-        project_id,
-        analysis_id,
-        "Output"
-    ).data[0].data_id
+    # Get output folder id
+    bclconvert_output_folder_id = (
+        get_analysis_output_object_from_analysis_output_code(
+            project_id,
+            analysis_id,
+            "Output"
+        ).data[0].data_id
+    )
 
     # Return all files recursively
     return bclconvert_output_folder_id, find_project_data_bulk(
@@ -97,28 +101,3 @@ def get_bclconvert_outputs_from_analysis_id(
         parent_folder_id=bclconvert_output_folder_id,
         data_type=FILE_DATA_TYPE
     )
-
-
-def get_bssh_json_file_id_from_analysis_output_list(analysis_output: List[ProjectData]) -> str:
-    """
-    Get the bssh json file id from the analysis output object
-    :param analysis_output:
-    :return:
-    """
-    return get_file_by_file_name_from_project_data_list("bsshoutput.json", analysis_output).data.id
-
-
-def get_run_info_xml_file_id_analysis_output_list(analysis_output: List[ProjectData]) -> str:
-    return get_file_by_file_name_from_project_data_list("RunInfo.xml", analysis_output).data.id
-
-
-def get_samplesheet_path_from_analysis_output_list(analysis_output: List[ProjectData]) -> str:
-    return get_file_by_file_name_from_project_data_list("SampleSheet.csv", analysis_output).data.details.path
-
-
-def get_samplesheet_file_id_from_analysis_output_list(analysis_output: List[ProjectData]) -> str:
-    return get_file_by_file_name_from_project_data_list("SampleSheet.csv", analysis_output).data.id
-
-
-def get_fastq_list_csv_file_id_from_analysis_output_list(analysis_output: List[ProjectData]) -> str:
-    return get_file_by_file_name_from_project_data_list("fastq_list.csv", analysis_output).data.id
